301ab5eea1b2a90e1fa2e86c461b01f06a0d1274
Ben Givertz <bgivertz@gmail.com>
2020-04-25 10:11:28 -0400

    test training

--- model/data.py
+++ model/data.py
@@ -1,9 +1,8 @@
 import os
 import numpy as np
 import cv2
-# from torch.autograd import Variable
-# from torchvision.utils import make_grid
-# import torch
+from torch.autograd import Variable
+import torch
 
 
 class ImagePair:
@@ -39,19 +38,17 @@ def load_data(path):
             image_t_path = os.path.join(img_folder_path, img_folder_name + "_0" + str(pair[1]) + ".jpg")
             lighting_s_path = os.path.join(img_folder_path, img_folder_name + "_light_0" + str(pair[0]) + ".txt")
             lighting_t_path = os.path.join(img_folder_path, img_folder_name + "_light_0" + str(pair[1]) + ".txt")
-            print(image_s_path)
-            print(image_t_path)
-            print(lighting_s_path)
-            print(lighting_t_path)
+            # print(image_s_path)
+            # print(image_t_path)
+            # print(lighting_s_path)
+            # print(lighting_t_path)
 
-            # I_s = get_image(image_s_path)
-            # I_t = get_image(image_t_path)
-            # L_s = get_lighting(lighting_s_path)
-            # L_t = get_lighting(lighting_t_path)
-            # img_pair = ImagePair(I_s, I_t, L_s, L_t)
-            # img_pairs.append(img_pair)
-            print("-----------------------------------------------------------")
-            print("-----------------------------------------------------------")
+            I_s = get_image(image_s_path)
+            I_t = get_image(image_t_path)
+            L_s = get_lighting(lighting_s_path)
+            L_t = get_lighting(lighting_t_path)
+            img_pair = ImagePair(I_s, I_t, L_s, L_t)
+            img_pairs.append(img_pair)
         
 
 
@@ -64,25 +61,25 @@ def load_data(path):
 #     img_pair = new ImagePair(I_s, I_t, L_s, L_t)
 #     return img_pair
 
-# def get_image(path_to_image):
-#     img = cv2.imread(path_to_image)
-#     # row, col, _ = img.shape
-#     # img = cv2.resize(img, (512, 512))
-#     img = cv2.resize(img, (128, 128)) #we changed this because of reduced image size
-#     Lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB) #converts image to one color space LAB
+def get_image(path_to_image):
+    img = cv2.imread(path_to_image)
+    Lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB) #converts image to one color space LAB
 
-#     inputL = Lab[:,:,0] #taking only the L channel
-#     inputL = inputL.astype(np.float32)/255.0 #normalise
-#     inputL = inputL.transpose((0,1))
-#     inputL = inputL[None,None,...] #not sure what's happening here
-#     inputL = Variable(torch.from_numpy(inputL).cuda())
+    inputL = Lab[:,:,0] #taking only the L channel
+    inputL = inputL.astype(np.float32)/255.0 #normalise
+    inputL = inputL.transpose((0,1))
+    inputL = inputL[None,None,...] #not sure what's happening here
+    inputL = Variable(torch.from_numpy(inputL).cuda())
     
-#     return inputL
+    return inputL
     
 
-# def get_lighting(path_to_light):
-#     sh = np.loadtxt(os.path.join(lightFolder, path_to_light))
-#     sh = sh[0:9]
-#     sh = sh * 0.7
+def get_lighting(path_to_light):
+    sh = np.loadtxt(path_to_light)
+    sh = sh[0:9]
+    sh = sh * 0.7
+    sh = np.squeeze(sh)
+    sh = np.reshape(sh, (1, 9, 1, 1)).astype(np.float32)
+    sh = Variable(torch.from_numpy(sh).cuda())
+    return sh
 
-load_data("")
