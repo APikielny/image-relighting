09494e81973cf7bab353a56b1bf0ff06665ef443
Aalia Habib <aalia_habib@brown.edu>
2020-04-25 20:06:01 +0500

    Updated data.py (2)

--- model/data.py
+++ model/data.py
@@ -44,12 +44,12 @@ def load_data(path):
             print(lighting_s_path)
             print(lighting_t_path)
 
-            # I_s = get_image(image_s_path)
-            # I_t = get_image(image_t_path)
-            # L_s = get_lighting(lighting_s_path)
-            # L_t = get_lighting(lighting_t_path)
-            # img_pair = ImagePair(I_s, I_t, L_s, L_t)
-            # img_pairs.append(img_pair)
+            I_s = get_image(image_s_path)
+            I_t = get_image(image_t_path)
+            L_s = get_lighting(lighting_s_path)
+            L_t = get_lighting(lighting_t_path)
+            img_pair = ImagePair(I_s, I_t, L_s, L_t)
+            img_pairs.append(img_pair)
             print("-----------------------------------------------------------")
             print("-----------------------------------------------------------")
         
@@ -58,31 +58,31 @@ def load_data(path):
     return np.asarray(img_pairs)
 
 # will make pulling multiple image pairs from the same folder nice
-# def make_image_pair(folder_path):
-#     #randomly select two images and their lighting
-#     #pass image paths to get_image, and lighting paths to get_lighting
-#     img_pair = new ImagePair(I_s, I_t, L_s, L_t)
-#     return img_pair
+def make_image_pair(folder_path):
+    #randomly select two images and their lighting
+    #pass image paths to get_image, and lighting paths to get_lighting
+    img_pair = new ImagePair(I_s, I_t, L_s, L_t)
+    return img_pair
 
-# def get_image(path_to_image):
-#     img = cv2.imread(path_to_image)
-#     # row, col, _ = img.shape
-#     # img = cv2.resize(img, (512, 512))
-#     img = cv2.resize(img, (128, 128)) #we changed this because of reduced image size
-#     Lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB) #converts image to one color space LAB
+def get_image(path_to_image):
+    img = cv2.imread(path_to_image)
+    # row, col, _ = img.shape
+    # img = cv2.resize(img, (512, 512))
+    img = cv2.resize(img, (128, 128)) #we changed this because of reduced image size
+    Lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB) #converts image to one color space LAB
 
-#     inputL = Lab[:,:,0] #taking only the L channel
-#     inputL = inputL.astype(np.float32)/255.0 #normalise
-#     inputL = inputL.transpose((0,1))
-#     inputL = inputL[None,None,...] #not sure what's happening here
-#     inputL = Variable(torch.from_numpy(inputL).cuda())
+    inputL = Lab[:,:,0] #taking only the L channel
+    inputL = inputL.astype(np.float32)/255.0 #normalise
+    inputL = inputL.transpose((0,1))
+    inputL = inputL[None,None,...] #not sure what's happening here
+    inputL = Variable(torch.from_numpy(inputL).cuda())
     
-#     return inputL
+    return inputL
     
 
-# def get_lighting(path_to_light):
-#     sh = np.loadtxt(os.path.join(lightFolder, path_to_light))
-#     sh = sh[0:9]
-#     sh = sh * 0.7
+def get_lighting(path_to_light):
+    sh = np.loadtxt(os.path.join(lightFolder, path_to_light))
+    sh = sh[0:9]
+    sh = sh * 0.7
 
 load_data("")
