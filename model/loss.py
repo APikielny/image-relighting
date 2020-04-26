3d0412d3f1c209e1aef4667980490e60699dad60
APikielny <apikielny@gmail.com>
2020-04-26 14:10:56 -0400

    batching

--- model/loss.py
+++ model/loss.py
@@ -1,50 +1,23 @@
 from skimage import filters
 import torch
 
-def L1(N, I_t, I_tp, L_s, L_sp):
+# def L1(N, I_t, I_tp, L_s, L_sp):
 
-    img_norm = torch.mean(torch.abs(I_t - I_tp))
-
-    #grad_norm = torch.norm(I_t.grad - I_tp.grad)
-
-    grad_norm = 0
-    if (I_t.grad != None):
-        print("I_t not none!")
-        grad_norm = torch.mean(torch.abs(I_t.grad - I_tp.grad))
-
-
-    # np_grad = filters.gaussian(I_t.cpu().detach().numpy()) - filters.gaussian(I_tp.cpu().detach().numpy())
-    # grad_norm = torch.norm(torch.from_numpy(np_grad))
-
-    
-    image_loss = img_norm + grad_norm
-
-    light_loss = torch.sum((L_s - L_sp) ** 2)
-
-
-    loss = image_loss + light_loss
-
-    
-    return loss
-
-#L1 loss for a whole batch
-# def L1_batch(N, I_t, I_tp, L_s, L_sp):
-
-#     img_norm = torch.sum(torch.mean(torch.abs(I_t - I_tp)))
+#     img_norm = torch.mean(torch.abs(I_t - I_tp))
 
 #     #grad_norm = torch.norm(I_t.grad - I_tp.grad)
 
-#     # grad_norm = 0
-#     # if (I_t.grad != None):
-#     #     print("I_t not none!")
-#     #     grad_norm = torch.mean(torch.abs(I_t.grad - I_tp.grad))
+#     grad_norm = 0
+#     if (I_t.grad != None):
+#         print("I_t not none!")
+#         grad_norm = torch.mean(torch.abs(I_t.grad - I_tp.grad))
 
 
 #     # np_grad = filters.gaussian(I_t.cpu().detach().numpy()) - filters.gaussian(I_tp.cpu().detach().numpy())
 #     # grad_norm = torch.norm(torch.from_numpy(np_grad))
 
     
-#     image_loss = img_norm #+ grad_norm
+#     image_loss = img_norm + grad_norm
 
 #     light_loss = torch.sum((L_s - L_sp) ** 2)
 
@@ -53,3 +26,40 @@ def L1(N, I_t, I_tp, L_s, L_sp):
 
     
 #     return loss
+
+#L1 loss for a whole batch
+def L1_batch(N, I_t, I_tp, L_s, L_sp):
+
+    print("loss function.")
+
+    print("all losses:", torch.mean(torch.abs(I_t - I_tp)))
+    print("all losses shape:", torch.mean(torch.abs(I_t - I_tp)).shape)
+
+    print("mean loss: ", torch.mean(torch.mean(torch.abs(I_t - I_tp))))
+
+    img_norm = torch.mean(torch.mean(torch.abs(I_t - I_tp)))
+
+    #grad_norm = torch.norm(I_t.grad - I_tp.grad)
+
+    # grad_norm = 0
+    # if (I_t.grad != None):
+    #     print("I_t not none!")
+    #     grad_norm = torch.mean(torch.abs(I_t.grad - I_tp.grad))
+
+
+    # np_grad = filters.gaussian(I_t.cpu().detach().numpy()) - filters.gaussian(I_tp.cpu().detach().numpy())
+    # grad_norm = torch.norm(torch.from_numpy(np_grad))
+
+    
+    image_loss = img_norm #+ grad_norm
+
+    print("all light loss:", (L_s - L_sp) ** 2)
+    light_loss = torch.sum((L_s - L_sp) ** 2)
+
+    print("light loss:", (L_s - L_sp) ** 2)
+
+
+    loss = image_loss + light_loss
+
+    
+    return loss
--- model/train.py
+++ model/train.py
@@ -68,21 +68,22 @@ def train(model, optimizer, data):
 
 
         skip_count = 4
-        print("I_s batch shape:", I_sbatch.shape)
-        print("L_t batch shape:", L_tbatch.shape)
+        # print("I_s batch shape:", I_sbatch.shape)
+        # print("L_t batch shape:", L_tbatch.shape)
+
         I_sbatch = torch.squeeze(I_sbatch, dim=1)
         L_tbatch = torch.squeeze(L_tbatch, dim=1)
 
         print("I_s batch shape squeeze:", I_sbatch.shape)
         print("L_t batch shape squeeze:", L_tbatch.shape)
 
-        I_tp, L_sp = model.forward(I_sbatch, L_tbatch, skip_count)
+        I_tp_batch, L_sp_batch = model.forward(I_sbatch, L_tbatch, skip_count)
 
-        # N = I_s.shape[0] * I_s.shape[0]
-        # loss = L1(N, I_t, I_tp, L_s, L_sp)
+        N = I_sbatch.shape[2] * I_sbatch.shape[2]
+        loss = L1_batch(N, I_tbatch, I_tp_batch, L_sbatch, L_sp_batch)
         # total_loss += loss
 
-        total_loss = total_loss / BATCH_SIZE
+        #total_loss = total_loss / BATCH_SIZE
         if (VERBOSE):
             print("Batch loss:", total_loss)
 
