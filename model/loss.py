95ac36a4ccd44b9de5a0c11e89007b0727ac8186
APikielny <apikielny@gmail.com>
2020-04-25 20:08:56 -0400

    loss testing

--- model/loss.py
+++ model/loss.py
@@ -6,7 +6,10 @@ def L1(N, I_t, I_tp, L_s, L_sp):
     img_norm = torch.mean(torch.abs(I_t - I_tp))
 
     #grad_norm = torch.norm(I_t.grad - I_tp.grad)
-    grad_norm = torch.mean(torch.abs(I_t.grad - I_tp.grad))
+
+    grad_norm = 0
+    if (I_t.grad != None):
+        grad_norm = torch.mean(torch.abs(I_t.grad - I_tp.grad))
 
 
     # np_grad = filters.gaussian(I_t.cpu().detach().numpy()) - filters.gaussian(I_tp.cpu().detach().numpy())
@@ -16,6 +19,7 @@ def L1(N, I_t, I_tp, L_s, L_sp):
     image_loss = img_norm + grad_norm
 
     light_loss = torch.sum((L_s - L_sp) ** 2)
+
 
     loss = image_loss + light_loss
 
