c0ad773a42ab75e35a285a579a0a836ec95ee268
APikielny <apikielny@gmail.com>
2020-04-26 11:54:48 -0400

    batch loss

--- model/loss.py
+++ model/loss.py
@@ -1,23 +1,49 @@
 from skimage import filters
 import torch
 
-def L1(N, I_t, I_tp, L_s, L_sp):
+# def L1(N, I_t, I_tp, L_s, L_sp):
 
-    img_norm = torch.mean(torch.abs(I_t - I_tp))
+#     img_norm = torch.mean(torch.abs(I_t - I_tp))
+
+#     #grad_norm = torch.norm(I_t.grad - I_tp.grad)
+
+#     grad_norm = 0
+#     if (I_t.grad != None):
+#         print("I_t not none!")
+#         grad_norm = torch.mean(torch.abs(I_t.grad - I_tp.grad))
+
+
+#     # np_grad = filters.gaussian(I_t.cpu().detach().numpy()) - filters.gaussian(I_tp.cpu().detach().numpy())
+#     # grad_norm = torch.norm(torch.from_numpy(np_grad))
+
+    
+#     image_loss = img_norm + grad_norm
+
+#     light_loss = torch.sum((L_s - L_sp) ** 2)
+
+
+#     loss = image_loss + light_loss
+
+    
+#     return loss
+
+def L1_batch(N, I_t, I_tp, L_s, L_sp):
+
+    img_norm = torch.sum(torch.mean(torch.abs(I_t - I_tp)))
 
     #grad_norm = torch.norm(I_t.grad - I_tp.grad)
 
-    grad_norm = 0
-    if (I_t.grad != None):
-        print("I_t not none!")
-        grad_norm = torch.mean(torch.abs(I_t.grad - I_tp.grad))
+    # grad_norm = 0
+    # if (I_t.grad != None):
+    #     print("I_t not none!")
+    #     grad_norm = torch.mean(torch.abs(I_t.grad - I_tp.grad))
 
 
     # np_grad = filters.gaussian(I_t.cpu().detach().numpy()) - filters.gaussian(I_tp.cpu().detach().numpy())
     # grad_norm = torch.norm(torch.from_numpy(np_grad))
 
     
-    image_loss = img_norm + grad_norm
+    image_loss = img_norm #+ grad_norm
 
     light_loss = torch.sum((L_s - L_sp) ** 2)
 
