fbb2a1038b217697d8987fa1352fec83d079a6b3
Ben Givertz <bgivertz@gmail.com>
2020-04-30 11:41:30 -0400

    test network debug

--- testNetwork.py
+++ testNetwork.py
@@ -125,9 +125,9 @@ for i in range(7):
     sh = np.reshape(sh, (1,9,1,1)).astype(np.float32)
     sh = Variable(torch.from_numpy(sh).cuda())
 
-    outputImg, outputSH  = my_network(inputL, sh, 0)
-    np_outputSH = outputSH.cpu().data.numpy()
-    render_half_sphere(np_outputSH, True)
+    outputImg, outputSH  = my_network.forward(inputL, sh, 0)
+    #np_outputSH = outputSH.cpu().data.numpy()
+    #render_half_sphere(np_outputSH, True)
 
     outputImg = outputImg[0].cpu().data.numpy()
     outputImg = outputImg.transpose((1,2,0))
