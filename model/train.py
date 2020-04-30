77bb944ed73bbf3c06ed5ece0293cf28e3bce5ca
Ben Givertz <bgivertz@gmail.com>
2020-04-30 14:08:33 -0400

    model naming

--- model/train.py
+++ model/train.py
@@ -113,7 +113,7 @@ for i in range(EPOCHS):
     print("Time elapsed to train epoch #", i + 1, ":", end - start)
 
 now = datetime.now()
-model_name = 'model_{}.pt'.format(now.strftime("%m-%d-%H%M"))
+model_name = 'model{}.pt'.format(now.strftime("%m-%d-%H%M"))
 
 print("Done training! Saving model as {}".format(model_name))
 torch.save(model.state_dict(), os.path.join('../trained_models/', model_name))
