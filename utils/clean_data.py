9d65348754da859ca6f2b20ba468093da5501c17
Ben Givertz <bgivertz@gmail.com>
2020-05-01 12:18:21 -0400

    clean data update

--- utils/clean_data.py
+++ utils/clean_data.py
@@ -1,39 +1,34 @@
 # Script to descend into a DPR folder, remove all excess
 # information and resize images.
 #
-# Usage: python clean_data.py <dir_path> <size>
+# Usage: python clean_data.py --dir <dir_path> --size <size> --save <save_path>
 # - dir_path: path to a DPR folder containing image folders
 # - size: size to rescale images to
-
-################################################################
-# CAUTION: Will permanently delete all unessesary files in dir #
-################################################################
+# - save_path: path to a folder to save all image folders
 
 from PIL import Image
-import os, sys
+import os, shutil
 import argparse
 
-def clean_data(path, size):
+def clean_data(path, size, save):
     # Loop through all folders in the dir
-    data_folders = os.listdir(path)
-    for df in data_folders:
-        df_path = os.path.join(path, df)
-        image_folders = os.listdir(df_path)
-        for imgf in image_folders:
-            imgf_path = os.path.join(df_path, imgf)
-            if os.path.isdir(imgf_path):
-                for item in os.listdir(imgf_path):
-                    i_path = os.path.join(imgf_path, item)
-                    i_name = os.path.basename(i_path)
-                    f, e = os.path.splitext(i_path)
-                    if imgf in i_name and (e == '.png'):
-                        im = Image.open(i_path)
-                        im = im.resize((int(size), int(size)))
-                        im.save(f + ".jpg", 'JPEG')
-                        os.remove(i_path)
-                    elif (imgf not in i_name) or ('log' in i_name):
-                        os.remove(i_path)
+    image_folders = os.listdir(path)
+    for imgf in image_folders:
+        imgf_path = os.path.join(path, imgf)
+        if os.path.isdir(imgf_path):
+            save_path = os.path.join(save, imgf)
+            if not os.path.exists(save_path):
+                os.makedirs(save_path)
+            for item in os.listdir(imgf_path):
+                i_path = os.path.join(imgf_path, item)
+                i_name = os.path.basename(i_path)
 
+                if (imgf in i_name) and ('.png' in i_name):
+                    im = Image.open(i_path)
+                    im = im.resize((size, size))
+                    im.save(os.path.join(save_path, (i_name[:len(i_name)-4] + ".jpg")), 'JPEG')
+                elif '_light_' in i_name:
+                    shutil.copyfile(i_path, os.path.join(save_path, i_name))
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -48,10 +43,16 @@ def parse_args():
         required=True,
         help='the directory containing all the data'
     )
+    parser.add_argument(
+        '--save',
+        required=True,
+        help='path to save data'
+    )
     return parser.parse_args()
 
 ARGS = parse_args()
-size = int(ARGS.size)
+size = ARGS.size
 path = ARGS.dir
+save  = ARGS.save
 
-clean_data(path, size)
+clean_data(path, size, save)
